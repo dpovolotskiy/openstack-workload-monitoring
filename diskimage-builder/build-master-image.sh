b1000b68996870a2bb34bb87b77912c0e3744483
Dmitriy Povolotskiy <povolotskiyda@yandex.ru>
2020-02-25 16:53:14 +0400

    Move to heat stack

---
+++ diskimage-builder/build-master-image.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export ARCH="amd64"
+export BASE_ELEMENTS="bootloader cloud-init-datasources ubuntu ubuntu-18-master"
+export DIB_CLOUD_INIT_DATASOURCES="ConfigDrive, Ec2"
+export DIB_RELEASE="bionic"
+export ELEMENTS_PATH="./elements/"
+export IMAGE_PATH="./images/ubuntu-18.04-master"
+export DIB_MIN_TMPFS=10
+
+disk-image-create vm $BASE_ELEMENTS -t qcow2 -o $IMAGE_PATH
---
+++ diskimage-builder/elements/ubuntu-18-slave/README.rst
@@ -0,0 +1,5 @@
+================
+ubuntu-18-master
+================
+
+Customize Ubuntu 18.04 (Bionic)
