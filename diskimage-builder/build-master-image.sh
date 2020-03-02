61d2fc44f1facf82a0104cfb79261498e8839958
Dmitriy Povolotskiy <povolotskiyda@yandex.ru>
2020-03-02 16:23:26 +0400

    Add extra-data.d to master image

--- diskimage-builder/build-master-image.sh
+++ diskimage-builder/build-master-image.sh
@@ -6,6 +6,5 @@ export DIB_CLOUD_INIT_DATASOURCES="ConfigDrive, Ec2"
 export DIB_RELEASE="bionic"
 export ELEMENTS_PATH="./elements/"
 export IMAGE_PATH="./images/ubuntu-18.04-master"
-export DIB_MIN_TMPFS=10
 
 disk-image-create vm $BASE_ELEMENTS -t qcow2 -o $IMAGE_PATH
